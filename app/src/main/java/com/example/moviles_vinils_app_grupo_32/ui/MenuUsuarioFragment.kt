5c5ee3ee88c4b7f97817a8064984a099d5d563a1
csolanor22 <c.solanor@uniandes.edu.co>
2022-11-05 13:33:43 -0500

    Add: Main menu and menu defaultUser (#43)

--- app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/MenuUsuarioFragment.kt
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/MenuUsuarioFragment.kt
@@ -8,20 +8,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.example.moviles_vinils_app_grupo_32.R
+import com.example.moviles_vinils_app_grupo_32.databinding.MenuUsuarioFragmentBinding
 
 class MenuUsuarioFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MenuUsuarioFragment()
-    }
-
+    private var _binding: MenuUsuarioFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.menu_usuario_fragment, container, false)
+        _binding = MenuUsuarioFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/UsersFragment.kt
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/UsersFragment.kt
@@ -8,11 +8,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.moviles_vinils_app_grupo_32.R
-import com.example.moviles_vinils_app_grupo_32.viewmodels.UsersViewModel
+import com.example.moviles_vinils_app_grupo_32.databinding.UsersFragmentBinding
 
 class UsersFragment : Fragment(), OnClickListener {
     var _navHostFragment: NavHostFragment? = null
@@ -24,16 +24,22 @@ class UsersFragment : Fragment(), OnClickListener {
     ): View? {
         _navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         _navController = _navHostFragment!!.navController
-        return inflater.inflate(R.layout.users_fragment, container, false)
+        val view = inflater?.inflate(R.layout.users_fragment,
+            container, false)
+        val userMenuButton: Button = view.findViewById(R.id.button)
+        userMenuButton.setOnClickListener(this);
+        val collectorMenuButton: Button = view.findViewById(R.id.button2)
+        userMenuButton.setOnClickListener(this);
+        collectorMenuButton.setOnClickListener(this)
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
-    }
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.button -> _navController?.navigate(R.id.action_usersFragment_to_menuUsuarioFragment)
+            R.id.button2 -> _navController?.navigate(R.id.action_usersFragment_to_menuCollectorFragment)
+        }
 
-    override fun onClick(p0: View?) {
-        _navController?.navigate(R.id.action_usersFragment_to_menuUsuarioFragment)
     }
 
 }
