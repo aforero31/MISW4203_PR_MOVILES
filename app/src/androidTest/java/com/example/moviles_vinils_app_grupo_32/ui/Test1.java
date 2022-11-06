1df7f706b08d8371b7718f53e8135405e1846f7b
Ana Lucia Forero <lucia.forero@bancoagrario.gov.co>
2022-11-06 13:04:41 -0500

    Validación test 2

--- app/src/androidTest/java/com/example/moviles_vinils_app_grupo_32/ui/Test1.java
+++ app/src/androidTest/java/com/example/moviles_vinils_app_grupo_32/ui/Test1.java
@@ -2,23 +2,24 @@ package com.example.moviles_vinils_app_grupo_32.ui;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 
 import static org.hamcrest.Matchers.allOf;
 
+import androidx.test.espresso.PerformException;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.example.moviles_vinils_app_grupo_32.R;
 
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,7 +40,17 @@ public class Test1 {
         usVisBtn.perform(click());
 
         //Validar como probar que existe resultado de la consulta
-        ViewInteraction imageView = onView(allOf(withId(R.id.imageView2),isDisplayed()));
+        ViewInteraction imageView = onView(allOf(withId(R.id.textView6), withText("Artista: Rubén Blades Bellido de Luna"))).check(matches(isDisplayed()));
         imageView.check(matches(isDisplayed()));
+    }
+
+    @Test(expected = PerformException.class)
+    public void itemWithText_doesNotExist() {
+        // Attempt to scroll to an item that contains the special text.
+        onView(ViewMatchers.withId(R.id.albumsRv))
+                // scrollTo will fail the test if no item matches.
+                .perform(RecyclerViewActions.scrollTo(
+                        hasDescendant(withText("not in the list"))
+                ));
     }
 }
