8e7d895b7e0ebbbb50aa21cdaa29b9a85fb7a562
andergcp <andergcp@hotmail.com>
2022-11-05 19:27:25 -0500

    Add: MusicianFragment al nav_graph

--- app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/MenuUsuarioFragment.kt
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/MenuUsuarioFragment.kt
@@ -1,18 +1,15 @@
 package com.example.moviles_vinils_app_grupo_32.ui
 
-import android.annotation.SuppressLint
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.moviles_vinils_app_grupo_32.R
-import com.example.moviles_vinils_app_grupo_32.databinding.MenuUsuarioFragmentBinding
 
 class MenuUsuarioFragment : Fragment(), OnClickListener {
     var _navHostFragment: NavHostFragment? = null
@@ -26,9 +23,9 @@ class MenuUsuarioFragment : Fragment(), OnClickListener {
         _navController = _navHostFragment!!.navController
         val view = inflater?.inflate(R.layout.menu_usuario_fragment, container, false)
         val userMenuButton: Button = view.findViewById(R.id.button3)
-        userMenuButton.setOnClickListener(this);
+        userMenuButton.setOnClickListener(this)
         val collectorMenuButton: Button = view.findViewById(R.id.button4)
-        userMenuButton.setOnClickListener(this);
+        userMenuButton.setOnClickListener(this)
         collectorMenuButton.setOnClickListener(this)
         return view
     }
@@ -36,13 +33,12 @@ class MenuUsuarioFragment : Fragment(), OnClickListener {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         activity?.actionBar?.title= getString(R.string.title_menu)
-
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.button3 -> _navController?.navigate(R.id.action_menuUsuarioFragment_to_albumFragment)
-            //R.id.button4 -> _navController?.navigate(R.id.action_usersFragment_to_menuCollectorFragment)
+            R.id.button4 -> _navController?.navigate(R.id.action_menuUsuarioFragment_to_musicianFragment2)
         }
     }
 }
