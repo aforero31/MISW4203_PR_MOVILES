a4ab41d6a9d849885bd08882b52f3745440f1248
Ana Lucia Forero <lucia.forero@bancoagrario.gov.co>
2022-11-17 21:59:22 -0500

    Construcción HU Musician_detail

---
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/adapters/MusicianDetailAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.moviles_vinils_app_grupo_32.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviles_vinils_app_grupo_32.R
+import com.example.moviles_vinils_app_grupo_32.models.Musician
+
+class MusicianDetailAdapter : RecyclerView.Adapter<MusicianDetailAdapter.MusicianDetailViewHolder>() {
+
+    var musician : Musician? = null
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicianDetailViewHolder {
+        val withDataBinding: MusicianDetailItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            MusicianDetailViewHolder.LAYOUT,
+            parent,
+            false)
+        return MusicianDetailViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: MusicianDetailViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.musician = musician
+        }
+    }
+
+    class MusicianDetailViewHolder(val viewDataBinding: MusicianDetailItemBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.musician_detail_item
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+}
---
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/viewmodels/MusicianDetailViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.moviles_vinils_app_grupo_32.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.moviles_vinils_app_grupo_32.models.Musician
+import com.example.moviles_vinils_app_grupo_32.repositories.MusicianDetailRepository
+
+class MusicianDetailViewModel (application: Application, musicianId: Int) :  AndroidViewModel(application){
+    private val musicianDetailRepository = MusicianDetailRepository(application)
+
+    private val _musician = MutableLiveData<Musician>()
+
+    val musician: LiveData<Musician>
+        get() = _musician
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    val id:Int = musicianId
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() {
+        musicianDetailRepository.refreshData(id, {
+            _musician.postValue(it)
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        },{
+            _eventNetworkError.value = true
+        })
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory (val app: Application, val musicianId: Int) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MusicianDetailViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return MusicianDetailViewModel(app, musicianId) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
