3769269b7fa26545c8f90686f9194988b219cc8d
andergcp <andergcp@hotmail.com>
2022-11-20 09:43:23 -0500

    Add: Collectors list implementation

--- app/src/main/java/com/example/moviles_vinils_app_grupo_32/network/NetworkServiceAdapter.kt
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/network/NetworkServiceAdapter.kt
@@ -9,6 +9,7 @@ import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.moviles_vinils_app_grupo_32.models.Album
+import com.example.moviles_vinils_app_grupo_32.models.Collector
 import com.example.moviles_vinils_app_grupo_32.models.Musician
 import com.example.moviles_vinils_app_grupo_32.models.Performers
 import org.json.JSONArray
@@ -142,6 +143,28 @@ class NetworkServiceAdapter constructor(context: Context) {
                 onError(it)
             }))
 
+    }
+
+    fun getCollectors(onComplete:(resp:List<Collector>)->Unit, onError: (error:VolleyError)->Unit){
+        requestQueue.add(getRequest("collectors",
+            Response.Listener<String> { response ->
+                val resp = JSONArray(response)
+                val list = mutableListOf<Collector>()
+                for (i in 0 until resp.length()) {
+                    val item = resp.getJSONObject(i)
+                    list.add(i,
+                        Collector(
+                            collectorId = item.getInt("id"),
+                            name = item.getString("name"),
+                            telephone = item.getString("telephone"),
+                            email = item.getString("email")
+                    ))
+                }
+                onComplete(list)
+            },
+            Response.ErrorListener {
+                onError(it)
+            }))
     }
 
     private fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
