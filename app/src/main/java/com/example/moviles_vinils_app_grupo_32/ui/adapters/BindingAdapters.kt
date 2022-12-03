44401bae36cd0a9ecfba44dd6c04a5d67ffc6ac0
csolanor22 <c.solanor@uniandes.edu.co>
2022-12-03 02:08:17 -0500

    Add: cache for images (#78)

--- app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/adapters/BindingAdapters.kt
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/ui/adapters/BindingAdapters.kt
@@ -4,12 +4,21 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import coil.load
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, cover: String?) {
-    cover?.let {
-        val imgUri = cover.toUri().buildUpon().scheme("https").build()
-        imgView.load(imgUri)
+
+    if (cover != null) {
+        Glide.with(imgView.context)
+            .load(cover.toUri().buildUpon().scheme("https").build())
+            .apply(
+                RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL))
+            .into(imgView)
     }
+
 }
 
