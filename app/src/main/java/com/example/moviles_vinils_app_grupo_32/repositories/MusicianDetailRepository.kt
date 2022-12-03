ec5b6ab40738e6bd8d990203d6988af256fab0d7
csolanor22 <c.solanor@uniandes.edu.co>
2022-12-03 13:39:19 -0500

    Add: cache to list and objects (#81)

--- app/src/main/java/com/example/moviles_vinils_app_grupo_32/repositories/MusicianDetailRepository.kt
+++ app/src/main/java/com/example/moviles_vinils_app_grupo_32/repositories/MusicianDetailRepository.kt
@@ -1,13 +1,23 @@
 package com.example.moviles_vinils_app_grupo_32.repositories
 
 import android.app.Application
+import android.util.Log
 import com.android.volley.VolleyError
+import com.example.moviles_vinils_app_grupo_32.models.Album
 import com.example.moviles_vinils_app_grupo_32.models.Musician
 import com.example.moviles_vinils_app_grupo_32.network.NetworkServiceAdapter
 
 class MusicianDetailRepository (val application: Application) {
     suspend fun refreshData(musicianId: Int): Musician {
-        //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
-        return NetworkServiceAdapter.getInstance(application).getMusician(musicianId)
+        var potentialResp = CacheManager.getInstance(application.applicationContext).getObjectFromCache("Musician$musicianId")
+        if(potentialResp==null){
+            Log.d("Cache decision", "get from network")
+            var musician = NetworkServiceAdapter.getInstance(application).getMusician(musicianId)
+            CacheManager.getInstance(application.applicationContext).addObjectToCache("Musician$musicianId", musician)
+            return musician
+        }
+        else{
+            return potentialResp as Musician
+        }
     }
 }
